f9da3226619e5218f322243992ff32333ba09349
RinatMadreiter <95545985+RinatMadreiter@users.noreply.github.com>
2026-03-30 14:10:32 +0200

    init nuxt

---
+++ nuxt.config.ts
@@ -0,0 +1,48 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  vite: {
+    optimizeDeps: {
+      include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit',
+      ]
+    }
+  },
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxt/content'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  mdc: {
+    highlight: {
+      noApiRoute: false
+    }
+  },
+
+  compatibilityDate: '2025-01-15',
+
+  nitro: {
+    prerender: {
+      routes: [
+        '/'
+      ]
+    }
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
+})
