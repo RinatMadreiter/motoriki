86daa664142c6fec98c3f61aaf68481d2a16c6b8
RinatMadreiter <95545985+RinatMadreiter@users.noreply.github.com>
2026-04-02 18:22:04 +0200

    removed columns in footer.vue

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,50 +1,11 @@
 <script setup lang="ts">
-const columns = [{
-  label: 'Resources',
-  children: [{
-    label: 'Help center'
-  }, {
-    label: 'Docs'
-  }, {
-    label: 'Roadmap'
-  }, {
-    label: 'Changelog'
-  }]
-}, {
-  label: 'Features',
-  children: [{
-    label: 'Affiliates'
-  }, {
-    label: 'Portal'
-  }, {
-    label: 'Jobs'
-  }, {
-    label: 'Sponsors'
-  }]
-}, {
-  label: 'Company',
-  children: [{
-    label: 'About'
-  }, {
-    label: 'Pricing'
-  }, {
-    label: 'Careers'
-  }, {
-    label: 'Blog'
-  }]
-}]
-
 
 </script>
 
 <template>
-  <USeparator
-    icon="i-simple-icons-nuxtdotjs"
-    class="h-px"
-  />
+  <USeparator icon="i-simple-icons-nuxtdotjs" class="h-px" />
 
   <UFooter :ui="{ top: 'border-b border-default' }">
-
     <template #left>
       <p class="text-sm text-muted">
         Built with Nuxt UI • © {{ new Date().getFullYear() }}
@@ -52,30 +13,12 @@ const columns = [{
     </template>
 
     <template #right>
-      <UButton
-        to="https://go.nuxt.com/discord"
-        target="_blank"
-        icon="i-simple-icons-discord"
-        aria-label="Nuxt on Discord"
-        color="neutral"
-        variant="ghost"
-      />
-      <UButton
-        to="https://go.nuxt.com/x"
-        target="_blank"
-        icon="i-simple-icons-x"
-        aria-label="Nuxt on X"
-        color="neutral"
-        variant="ghost"
-      />
-      <UButton
-        to="https://github.com/nuxt-ui-templates/landing"
-        target="_blank"
-        icon="i-simple-icons-github"
-        aria-label="Nuxt UI on GitHub"
-        color="neutral"
-        variant="ghost"
-      />
+      <UButton to="https://go.nuxt.com/discord" target="_blank" icon="i-simple-icons-discord"
+        aria-label="Nuxt on Discord" color="neutral" variant="ghost" />
+      <UButton to="https://go.nuxt.com/x" target="_blank" icon="i-simple-icons-x" aria-label="Nuxt on X" color="neutral"
+        variant="ghost" />
+      <UButton to="https://github.com/nuxt-ui-templates/landing" target="_blank" icon="i-simple-icons-github"
+        aria-label="Nuxt UI on GitHub" color="neutral" variant="ghost" />
     </template>
   </UFooter>
 </template>
